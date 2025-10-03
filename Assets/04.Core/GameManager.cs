0e523b71c2e6f0d5ce27f93a2550742cf67262bf
dongho <jijiji0809160@gmail.com>
2025-10-03 22:06:33 +0900

    feat : GameManager

---
+++ Assets/04.Core/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using Assets._01.Member.CDH.Code.Events;
+using NUnit.Framework;
+using System;
+using UnityEngine;
+
+namespace Assets._04.Core
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private EventChannelSO uiEventChannel;
+        [SerializeField] private EventChannelSO turnManagerChannel;
+
+        private void Awake()
+        {
+            turnManagerChannel.AddListener<DrawCardsStartEvent>(HandleDrawCardStart);
+        }
+
+        private void OnDestroy()
+        {
+            turnManagerChannel.RemoveListener<DrawCardsStartEvent>(HandleDrawCardStart);
+        }
+
+        private void HandleDrawCardStart(DrawCardsStartEvent evt)
+        {
+            
+        }
+    }
+}
